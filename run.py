cd32268eb3704a793ce67769cf5764d2f156a40a
FilipHarald <Filip.harald@gmail.com>
2015-09-30 10:25:34 +0200

    refined URL-methods

--- run.py
+++ run.py
@@ -56,21 +56,36 @@ def render_unicorn_page(id):
 
     lat = str(unicorn_dict.get("spottedWhere").get("lat"))
     lon = str(unicorn_dict.get("spottedWhere").get("lon"))
-    hotel_radius = str(7000)
+    nearby_res_list = get_nearby_lodges_list(lat, lon)
+
+    print(str(nearby_res_list[0].get("name")))
+
+    return template("unicorn", unicorn=unicorn_dict)
+
+
+def get_nearby_lodges_list(lat, lon):
+    lodges_radius = 7000
+    nearby_url = get_nearby_lodges_url(lat, lon, lodges_radius)
+    nearby_req = requests.get(nearby_url)
+    nearby_response_json = nearby_req.json()
+    while nearby_response_json.get("results").size == 0:
+        nearby_url = get_nearby_lodges_url(lat, lon, (lodges_radius + 7000))
+        nearby_req = requests.get(nearby_url)
+        nearby_response_json = nearby_req.json()
+        print(nearby_url)
+    print("Final URL: " + nearby_url)
+    return nearby_url
+
+
+def get_nearby_lodges_url(lat, lon, radius):
     types = "lodging"
     key = "AIzaSyCJhyHp-740GGvy4bBLJatNOIOnru-4hfA"
     nearby_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + \
                  "location=" + lat + "," + lon + "&" + \
-                 "radius=" + hotel_radius + "&" + \
+                 "radius=" + str(radius) + "&" + \
                  "types=" + types + "&" + \
                  "key=" + key
-
-    print(nearby_url)
-    nearby_req = requests.get(nearby_url)
-    nearby_res_list = nearby_req.json().get("results")
-    print(str(nearby_res_list[0].get("name")))
-    return template("unicorn", unicorn=unicorn_dict)
-
+    return nearby_url
 
 #command for running the service local.    
 run(host='localhost', port=8080, debug=True, reloader=True)
